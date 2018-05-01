6d53f479a7b67dae2f3c18975268b25d36224b17
huihuimoe <0w0@loli.pet>
2018-05-01 17:56:17 +0800

    use native method not  regexp

--- libs/convert.js
+++ libs/convert.js
@@ -1,9 +1,7 @@
-const r = /\d+(\.\d+)?/
-
 module.exports = s => {
   s = s.toLowerCase()
-  if (s.endsWith('kb') || s.endsWith('kib')) { return +s.match(r)[0] / 1024 / 1024 }
-  if (s.endsWith('mb') || s.endsWith('mib')) { return +s.match(r)[0] / 1024 }
-  if (s.endsWith('gb') || s.endsWith('gib')) { return +s.match(r)[0] }
-  if (s.endsWith('tb') || s.endsWith('tib')) { return +s.match(r)[0] * 1024 }
+  if (s.endsWith('kb') || s.endsWith('kib')) { return Number.parseFloat(s) / 1024 / 1024 }
+  if (s.endsWith('mb') || s.endsWith('mib')) { return Number.parseFloat(s) / 1024 }
+  if (s.endsWith('gb') || s.endsWith('gib')) { return Number.parseFloat(s) }
+  if (s.endsWith('tb') || s.endsWith('tib')) { return Number.parseFloat(s) * 1024 }
 }
