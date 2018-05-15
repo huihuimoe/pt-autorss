ae7132497de5d998503af5caa9db1c734211f72c
huihuimoe <0w0@loli.pet>
2018-05-16 00:45:41 +0800

    add parse-torrent

--- plugins/deluge.js
+++ plugins/deluge.js
@@ -1,9 +1,13 @@
 const { execSync } = require('child_process')
+const parseTorrent = require('parse-torrent')
+const fs = require('fs')
 
 // need deluge-console
 module.exports = () => {
   return (outputFilename, name) => {
     console.log(name, ' downloaded completely. Pushing...')
     execSync(`deluge-console add ${outputFilename}`)
+    const {infoHash} = parseTorrent(fs.readFileSync(outputFilename))
+    execSync(`deluge-console info ${infoHash}`)
   }
 }
