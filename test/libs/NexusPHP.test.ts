6404f0b155b67593c24887a8ce3130114f45395a
huihuimoe <0w0@loli.pet>
2019-03-12 19:09:12 +0800

    test: init unit tests and add libs/helper tests

---
+++ test/libs/NexusPHP.test.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+import sinon = require('sinon')
+import NexusPHP from '../../libs/NexusPHP'
+
+describe('libs/NexusPHP', () => {
+  it('Status', done => {
+    // @TODO
+    // ↓ BUGGGGGGGGGGG!
+    // class TestClass extends NexusPHP {}
+    done()
+  })
+})
