75108461b4deaac805cd7fea42f31547cef735b4
huihuimoe <0w0@loli.pet>
2019-03-28 19:06:20 +0800

    feat: add qBittorrent plugins

--- plugins/qBittorrent.ts
+++ plugins/qBittorrent.ts
@@ -1,11 +1,36 @@
-import { connect } from 'qbittorrent-api'
-import { IDownloadToRetn } from '../bootstrap'
+import { addOptions as IaddOptions, connect } from 'qbittorrent-api'
+import { IRequireStatus } from '../typings/application'
 
-export default () => {
-  // @TODO
-  return (file: IDownloadToRetn) => {
-    console.log(`Pushing ${file.filename} to qBittorrent using qBittorrent API...`)
-    // @TODO
-    return file
+export interface IqBittorrentConnectOptions {
+  host: string
+  username: string
+  password: string
+}
+
+export default (connectOptions: IqBittorrentConnectOptions, addOptions: IaddOptions) => {
+  const instance = connect(
+    connectOptions.host,
+    connectOptions.username,
+    connectOptions.password,
+  )
+  return async (status: IRequireStatus) => {
+    // test is login
+    await new Promise((resolve, rejects) => {
+      instance.transferInfo(e => {
+        // relogin
+        if (e) instance.reconnect()
+        resolve()
+      })
+    })
+
+    console.log(`Pushing ${status.id} to qBittorrent using qBittorrent API...`)
+    await new Promise((resolve, rejects) => {
+      instance.add2(status.downLink, addOptions, e => {
+        if (e) rejects(e)
+        resolve()
+      })
+    })
+
+    return status
   }
 }
