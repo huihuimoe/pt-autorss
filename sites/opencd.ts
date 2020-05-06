3a8b170acc15cbad84c3ec1d133363f87055d4d4
huihuimoe <0w0@loli.pet>
2020-05-06 13:32:28 +0800

    fix: opencd changed

--- sites/opencd.ts
+++ sites/opencd.ts
@@ -1,17 +1,14 @@
+import dayjs = require('dayjs')
+import { get } from 'lodash'
+import { convert } from '../libs/helper'
 import NexusPHP from '../libs/NexusPHP'
 
 export default class OPENCD extends NexusPHP {
-  public getStatus(element: Element) {
-    return {
-      ...super.getStatus(element),
-      get isSticky() {
-        return !!element.querySelector('.sticky')
-      },
-    }
-  }
   public async preAnalyze(document: Document) {
     await super.preAnalyze(document)
-    for (const e of document.querySelectorAll<HTMLTableRowElement>('.torrents > tbody > tr'))
+    for (const e of document.querySelectorAll<HTMLTableRowElement>('.torrents > tbody > tr')) {
+      e.children[1].remove()
       e.children[2].remove()
+    }
   }
 }
